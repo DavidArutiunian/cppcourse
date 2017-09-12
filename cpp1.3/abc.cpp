33c2b1189423b0d88ab9ef5731240506e855d549
David Arutiunian <arutunjan666@mail.com>
2017-09-12 20:12:16 +0300

    add task 1.3

---
+++ cpp1.3/abc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    float a = 0;
+    float b = 0;
+    float c = 0;
+
+    cout << "Enter a, b and c: ";
+    cin >> a >> b >> c;
+
+    float result = a + b + c;
+    cout << "Total: " << result << endl;
+}
