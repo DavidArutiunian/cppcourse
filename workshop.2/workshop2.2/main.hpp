3483c6c78a83fe6645f659b710a5ccae72bb78e0
David Arutiunian <arutunjan666@mail.com>
2017-10-20 21:18:58 +0300

    refactor code

--- workshop.2/workshop2.2/main.hpp
+++ workshop.2/workshop2.2/main.hpp
@@ -8,16 +8,16 @@
 #include <windows.h>
 
 static const unsigned BALLS_COUNT = 4;
-static const char *WINDOW_TITLE = "Moving Balls v2.1";
+static const char* WINDOW_TITLE = "Moving Balls v2.0";
 static const unsigned ANTIALIASING_LEVEL = 8;
 static const unsigned MAX_FPS = 60;
 static const unsigned WINDOW_WIDTH = 800;
 static const unsigned WINDOW_HEIGHT = 600;
 
-void init(std::vector<Ball> &balls);
+void init(std::vector<Ball>& balls);
 
-void pollEvents(sf::RenderWindow &window);
+void pollEvents(sf::RenderWindow& window);
 
-void update(std::vector<Ball> &balls, float deltaTime);
+void update(std::vector<Ball>& balls, float deltaTime);
 
-void redrawFrame(sf::RenderWindow &window, std::vector<Ball> &balls);
+void redrawFrame(sf::RenderWindow& window, std::vector<Ball>& balls);
